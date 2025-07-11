bcd8cdf9dc735aabe7cdebd11fa6ada6eeff47cf
huacishu <18265406260@163.com>
2025-07-11 16:41:49 +0800

    Flink重新学习

--- flink_code/src/main/java/flink_demo/sink/SinkFile.java
+++ flink_code/src/main/java/flink_demo/sink/SinkFile.java
@@ -1,5 +1,75 @@
-package flink_demo.sink;/**
+package flink_demo.sink;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringEncoder;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.connector.source.util.ratelimit.RateLimiterStrategy;
+import org.apache.flink.configuration.MemorySize;
+import org.apache.flink.connector.datagen.source.DataGeneratorSource;
+import org.apache.flink.connector.datagen.source.GeneratorFunction;
+import org.apache.flink.connector.file.sink.FileSink;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.introspect.DefaultAccessorNamingStrategy;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.filesystem.BucketAssigner;
+import org.apache.flink.streaming.api.functions.sink.filesystem.OutputFileConfig;
+import org.apache.flink.streaming.api.functions.sink.filesystem.bucketassigners.DateTimeBucketAssigner;
+import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
+
+import java.time.Duration;
+import java.time.ZoneId;
+
+/**
  * @Auther:huacishu
  * @Date: 2025/4/7
- */public class SinkFile {
+ */
+public class SinkFile {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        //必须开启checkpoint 否则一直都是inprocess
+       env.enableCheckpointing(1000, CheckpointingMode.EXACTLY_ONCE);
+
+        DataGeneratorSource<String> dataGeneratorSource = new DataGeneratorSource<>(
+                new GeneratorFunction<Long, String>() {
+                    @Override
+                    public String map(Long value) throws Exception {
+                        return "Number" + value;
+                    }
+                },
+                1000,
+                RateLimiterStrategy.perSecond(10),
+                Types.STRING
+        );
+
+        DataStreamSource<String> dataGen = env.fromSource(dataGeneratorSource, WatermarkStrategy.noWatermarks(), "data-gen");
+
+        //TODO 输出到文件系统
+        FileSink<String> fileSink = FileSink
+                //输出行式存储的文件，指定路径，指定编码
+                .<String>forRowFormat(new Path("/tmp"), new SimpleStringEncoder<String>("UTF-8"))
+                //输出文件的一些配置，文件名、前后缀、压缩方式等
+                .withOutputFileConfig(OutputFileConfig.builder()
+                        .withPartPrefix("huacishu")
+                        .withPartSuffix(".txt")
+                        .build()
+                )
+                //按照目录文件分桶
+                .withBucketAssigner(new DateTimeBucketAssigner<>("yyyy-MM-dd HH", ZoneId.of("Asia/Shanghai")))
+                //文件的滚动策略 根据条件生产  滚动时间为 10秒 大小为1M
+                .withRollingPolicy(
+                        DefaultRollingPolicy.builder()
+                                .withRolloverInterval(Duration.ofSeconds(10))
+                                .withMaxPartSize(new MemorySize(1024 * 1024))
+                                .build()
+                )
+                .build();
+
+        dataGen.sinkTo(fileSink);
+
+        env.execute();
+    }
 }
--- flink_code/src/main/java/flink_demo/sink/SinkKafka.java
+++ flink_code/src/main/java/flink_demo/sink/SinkKafka.java
@@ -1,5 +1,61 @@
-package flink_demo.sink;/**
+package flink_demo.sink;
+
+import flink_demo.bean.WaterSensor;
+import flink_demo.function.WaterSensorMapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.base.DeliveryGuarantee;
+import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.kafka.clients.producer.ProducerConfig;
+
+import static org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.OnCheckpointRollingPolicy.build;
+
+/**
  * @Auther:huacishu
  * @Date: 2025/4/8
- */public class SinkKafka {
+ */
+public class SinkKafka {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(2);
+
+        //如果是精准一次，必须开启checkpoint
+        env.enableCheckpointing(1000, CheckpointingMode.EXACTLY_ONCE);
+
+
+        SingleOutputStreamOperator<String> sensorDS = env.socketTextStream("hadoop102", 7777);
+                //.map(new WaterSensorMapFunction());
+/**
+ *  kafka Sink
+ *  注意：如果要使用 精准一次写入到Kafka 需满足以下条件，缺一不可
+ *  1、开启checkpoint
+ *  2、设置事务前缀
+ *  3、设置事务超时时间  checkpoint间隔 < 事务超时时间  < max 的15分钟
+ *
+ */
+
+
+        KafkaSink<String> kafkaSink = KafkaSink.<String>builder()
+                //指定kafka 的地址和端口
+                .setBootstrapServers("")
+                //指定序列化器：指定topic名称，具体的序列化器
+                .setRecordSerializer(KafkaRecordSerializationSchema.<String>builder()
+                        .setTopic("sink_topic")
+                        .setValueSerializationSchema(new SimpleStringSchema())
+                        .build()
+                )
+                //写到kafka的一致性级别 ： 精准一次，至少一次
+                .setDeliveryGuarantee(DeliveryGuarantee.EXACTLY_ONCE)
+                //如果是精准一次，必须设置事务的前缀
+                .setTransactionalIdPrefix("huacishu") //设置事务id的前缀
+                //如果是精准一次，必须设置 事务超时时间：大于checkpoint间隔 小于Max15分钟
+                .setProperty(ProducerConfig.TRANSACTION_TIMEOUT_CONFIG,10*60*1000+"")
+                .build();
+
+
+        env.execute();
+    }
 }
